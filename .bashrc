1f5fd76ab93fd1db0d6f2fcbe604d16331ebd73e
Ryan Reaves <rbreaves@gmail.com>
2016-05-08 00:06:20 -0500

    Added the shell commands to install the autocopy fix for gnome-terminal, and added the commands to append the changes to .bashrc

---
+++ .bashrc
@@ -0,0 +1,7 @@
+stty intr ^X
+
+if pgrep -f "python /home/user/.autocopy.py" &>/dev/null; then
+    :
+else
+    nohup python -u ~/autocopy.py &> nohup.out &
+fi
--- install.sh
+++ install.sh
@@ -3,30 +3,27 @@
 # For Ubuntu only
 # Tested only on 16.04
 
-#Swapping Alt, Win and Ctrl keys to align with OSX - but the Ctrl key will act as the Super (Win) key
-gsettings set org.gnome.desktop.input-sources xkb-options ['altwin:ctrl_win', 'ctrl:swap_lalt_lctl_lwin']
+bashrc=/home/`whoami`/.bashrc
+newbashrc=./.bashrc
 
-# Reset the termination shortcut from Ctrl-C to Ctrl-X, Ctrl-C cannot be mapped to from the Super (Win) key
-# When we add keyboard shortcuts Super-C will effectively be Ctrl-C on your physical keyboard
-# Add logic for checking for remap in .bashrc
-# then add this if missing
-#
-# stty intr ^X
+#Swapping Alt, Win and Ctrl keys to align with OSX - but the Ctrl key will act as the Super (Win) key
+gsettings set org.gnome.desktop.input-sources xkb-options "['altwin:ctrl_win', 'ctrl:swap_lalt_lctl_lwin']"
+
+#Adding python script to enable autocopy on gnome-terminal
+cp autocopy.py ~/.autocopy.py
+
+# 1. Initiating python script from bashrc
+# 2. Reset the termination shortcut from Ctrl-C to Ctrl-X, Ctrl-C cannot be mapped to from the Super (Win) key
+# Note: When we add keyboard shortcuts Super-C will effectively be Ctrl-C on your physical keyboard
+sed -i -e "s/user/`whoami`/g" ./.bashrc
+if [ `fgrep -x -f ~/.bashrc .bashrc | wc -l` -ne 7 ]; then
+	cat "$newbashrc" >> "$bashrc"
+fi
+
+read -p 'The script has installed OSX keymaps, and the autocopy python script. Press any key to finish...'
+
 
 # Automate the keyboard shortcuts needed in Ubuntu
 # file shortcuts.py
 # python3 shortcuts.py 'Take a screenshot of area' 'gnome-screenshot -a' '<Shift><Ctrl>$'
 # python3 shortcuts.py 'Control C' "xte 'keydown Control_L' 'key x' 'keyup Control_L' 'keydown Control_L' 'key c' 'keyup Control_L'" '<Super>C'
-
-# Add more python code here to setup the auto copy selection feature in terminal to match OSX behavior
-# file autocopy.py
-# Add logic in .bashrc
-# If gnome terminal has the focus python will run autocutsel
-# else the script will copy the contents from autocutsel clipboard, kill autocutsel then place the data back into the clipboard
-# Apparently the clipboard wipes itself out once autocutsel is killed, so python has to grab it first to put it back in
-#
-# if pgrep -f "python /home/{user}/autocopy.py" &>/dev/null; then
-#     :
-# else
-#     nohup python ~/autocopy.py &> nohup.out &
-# fi
