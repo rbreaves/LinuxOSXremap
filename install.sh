4914c2961df7ce407af294983d9450558a4cf3b8
Ryan Reaves <rbreaves@gmail.com>
2016-05-08 23:09:15 -0500

    Fixed the append on the symbols ctrl file, removed xkb-option that did not belong and may cause unity's gui to crash

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 
 # Add right alt and ctrl swap
 if [[ ! `grep 'swap_ralt_rctrl' /usr/share/X11/xkb/symbols/ctrl` ]]; then
-	sudo cat ./symbols_ctrl >> /usr/share/X11/xkb/symbols/ctrl
+	sudo cat ./symbols_ctrl | sudo tee --append /usr/share/X11/xkb/symbols/ctrl > /dev/null
 	rebootnow=true
 fi
 
@@ -33,7 +33,7 @@ if [[ ! `grep 'swap_ralt_rctrl' /usr/share/X11/xkb/rules/evdev.lst` ]]; then
 fi
 
 #Swapping Alt, Win and Ctrl keys to align with OSX - but the Ctrl key will act as the Super (Win) key
-gsettings set org.gnome.desktop.input-sources xkb-options "['altwin:ctrl_win', 'ctrl:swap_lalt_lctl_lwin', 'ctrl:swap_ralt_rctrl']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lalt_lctl_lwin', 'altwin: swap_alt_win', 'ctrl:swap_ralt_rctrl']"
 
 #Adding python script to enable autocopy on gnome-terminal
 cp autocopy.py ~/.autocopy.py
